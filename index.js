c95a79ab6fcc7569df38e7eab48de80c5c88678d
nirjhor saha <nirjhor.badhan25@gmail.com>
2025-09-04 21:02:28 +0600

    Completed practicum implementation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,7 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_TOKEN;
+// const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_TOKEN;
+const CUSTOM_OBJECT = process.env.CUSTOM_OBJECT; 
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -61,10 +65,65 @@ app.post('/update', async (req, res) => {
         res.redirect('back');
     } catch(err) {
         console.error(err);
-    }
+    }a
 
 });
 */
+
+// ROUTE 1 - Homepage (list all custom object records)
+app.get("/", async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT}?properties=name&properties=description&properties=category`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        "Content-Type": "application/json",
+    };
+
+    try {
+        const resp = await axios.get(url, { headers });
+        // console.log("Full Response:", JSON.stringify(resp.data, null, 2));
+        res.render("homepage", { title: "Homepage", data: resp.data.results || [] });
+    } catch (error) {
+        console.error(
+            "Error fetching custom object data:",
+            error.response?.data || error.message,
+        );
+        res.render("homepage", { title: "Homepage", data: [] });
+    }
+});
+
+
+
+// ROUTE 2 - Form page for creating new record
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' 
+    });
+});
+
+// ROUTE 3 - Handle form submission
+app.post('/update-cobj', async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category
+        }
+    };
+
+    try {
+        await axios.post(url, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating custom object record:', error.response?.data || error.message);
+        res.status(500).send('Error creating record');
+    }
+});
 
 
 // * Localhost
